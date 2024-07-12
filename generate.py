070023cf8428bdffffb0a65b0c2d94623784b9a2
Zhao Lunqi <1085789239@qq.com>
2024-07-12 12:37:31 +0800

    修改流量生成脚本，来适应packet scaling参数

--- generate.py
+++ generate.py
@@ -7,6 +7,13 @@ file_path = 'trace_burst.csv'
 # Define the number of rows to generate
 num_rows = 10000
 
+# 计算x离n的最近的整数倍
+def approximate_multiple(x, n):
+    return round(x / n) * n
+
+# 当前VPP模拟环境中，一个Packet对象对应的真实包个数
+packet_obj_scaling = 2
+
 # Open the file in write mode
 with open(file_path, 'w', newline='') as file:
     # Create a CSV writer object
@@ -26,6 +33,7 @@ with open(file_path, 'w', newline='') as file:
         # 第一段，高流量
         for i in range(num_rows):
             packet_num = random.randint(5, 12)
+            packet_num = approximate_multiple(packet_num, packet_obj_scaling)
             ratio = random.uniform(0, 1)
             # if i % 20 == 0:
             #     packet_num = random.randint(20,10)
@@ -34,19 +42,23 @@ with open(file_path, 'w', newline='') as file:
         # 第一段，低流量
         for i in range(num_rows):
             packet_num = random.randint(0, 5)
+            packet_num = approximate_multiple(packet_num, packet_obj_scaling)
             ratio = random.uniform(0, 1)
             writer.writerow([packet_num, ratio])
         # 第三段，burst高流量
         for i in range(num_rows):
             packet_num = random.randint(0, 3)
+            packet_num = approximate_multiple(packet_num, packet_obj_scaling)
             ratio = random.uniform(0, 1)
             if i % 8 == 0:
                 packet_num = random.randint(20,30)
+                packet_num = approximate_multiple(packet_num, packet_obj_scaling)
                 ratio = random.uniform(0, 1)
             writer.writerow([packet_num, ratio])
         # 第四段，低流量
         for i in range(num_rows):
             packet_num = random.randint(0, 4)
+            packet_num = approximate_multiple(packet_num, packet_obj_scaling)
             ratio = random.uniform(0, 1)
             writer.writerow([packet_num, ratio])
 
